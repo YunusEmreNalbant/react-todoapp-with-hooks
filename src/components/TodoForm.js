d3817e5c6705170bcd939d3000d8cace77104768
YunusEmreNalbant <nalbantyunusemre@hotmail.com>
2020-10-12 14:39:09 +0300

    Editing TodoForm Component

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function TodoForm() {
+function TodoForm(props) {
+    const [input, setInput] = useState('');
+
+    const handleChange = (e) => {
+        setInput(e.target.value);
+    }
+
+    const handleSubmit = (e) => {
+        console.log(props);
+        e.preventDefault();
+
+        // props.onSubmit({
+        //     id: Math.floor(Math.random() * 10000),
+        //     text: input
+        // });
+
+        setInput('');
+
+    }
     return (
-        <div>
-            
-        </div>
+        <form action="todo-form" onSubmit={handleSubmit}>
+            <input type="text" placeholder="Add a todo" value={input} name="text" className="todo-input" onChange={handleChange} />
+            <button className="todo-button">Add todo</button>
+
+        </form>
     )
 }
 
-export default TodoForm
+export default TodoForm 
